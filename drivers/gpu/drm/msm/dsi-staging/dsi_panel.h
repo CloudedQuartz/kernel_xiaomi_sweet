d11f19a0ddd8bd94cb6158559141264b5ee9fb30
Adithya <gh0strider.2k18.reborn@gmail.com>
2021-10-07 14:10:48 +0530

    dsm/msm: dsi-staging: Add support for CABC mode
     * Content Adaptive Backlight Control
     * borrowed code from HBM
    
    Change-Id: I97d8500a81d5b0079f01757ec07cc94ca32af937
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -260,6 +260,8 @@ struct dsi_panel {
 
 	int hbm_mode;
 
+        int cabc_mode;
+
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
 
@@ -402,5 +404,7 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
 int dsi_panel_apply_dc_mode(struct dsi_panel *panel);
+
+int dsi_panel_apply_cabc_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
